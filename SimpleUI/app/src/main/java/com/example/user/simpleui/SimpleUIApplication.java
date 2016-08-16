3fb33d419721ac6173197854d6e4107fbbbb15dd
HermioneLo <iss5010912@gmail.com>
2016-08-16 12:15:45 +0800

    read and write file & parse initialization

---
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/SimpleUIApplication.java
@@ -0,0 +1,23 @@
+package com.example.user.simpleui;
+
+import android.app.Application;
+
+import com.parse.Parse;
+
+import java.util.ArrayList;
+
+/**
+ * Created by user on 2016/8/16.
+ */
+public class SimpleUIApplication extends Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("3HhmfC5AyW17hTlFPPQRyaAbP8OVd9xTfFe7VVDg")
+                .server("https://parseapi.back4app.com/")
+                .clientKey("EdZCyuwNIKyzmNcyhYuucwo5bpEsn1MwCXABlbom")
+                .build()
+        );
+    }
+}
