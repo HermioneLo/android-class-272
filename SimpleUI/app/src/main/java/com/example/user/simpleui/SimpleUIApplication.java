c94e6e7725f5fabab271476b010b69b1c9f92b2a
HermioneLo <iss5010912@gmail.com>
2016-08-17 10:26:44 +0800

    finish downloading drink from remote

--- SimpleUI/app/src/main/java/com/example/user/simpleui/SimpleUIApplication.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/SimpleUIApplication.java
@@ -3,6 +3,7 @@ package com.example.user.simpleui;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 import java.util.ArrayList;
 
@@ -13,6 +14,9 @@ public class SimpleUIApplication extends Application{
     @Override
     public void onCreate() {
         super.onCreate();
+
+        ParseObject.registerSubclass(Drink.class);
+        
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("3HhmfC5AyW17hTlFPPQRyaAbP8OVd9xTfFe7VVDg")
                 .server("https://parseapi.back4app.com/")
